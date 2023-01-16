13576a583f6881ca6ed3f9d9412aa0ea340d8f55
Katrina <katrinalea.woolley@yahoo.co.uk>
2023-01-16 14:07:49 +0000

    interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,15 +1,39 @@
-
 export interface IResource {
-    id: number;
-    url: string;
-    author: string;
-    title: string;
-    desc: string;
-    tags: string[];
-    type: string;
-    week: number;
-    timeStamp: string;
-    userid: number;
-    usedreview: string;
-    reason: string;
+  resourceid: number;
+  url: string;
+  author: string;
+  title: string;
+  desc: string;
+  tags: string[];
+  type: string;
+  week: number;
+  timeStamp: string;
+  userid: number;
+  usedreview: string;
+  reason: string;
+}
+
+export interface ILike {
+  resourceid: number;
+  userid: number;
+  liked: boolean;
+}
+
+export interface IComment {
+  resourceid: number;
+  commmentid: number;
+  userid: number;
+  comment: string;
+}
+
+export interface IUser {
+  userid: number;
+  username: string;
+  status: string;
+}
+
+export interface IToDo {
+  itemid: number;
+  userid: number;
+  resourceid: number;
 }
