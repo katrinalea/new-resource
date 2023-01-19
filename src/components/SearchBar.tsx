d19e68652293a32b61346df2a85546dd3ee18e4a
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-19 13:11:56 +0000

    working searchBar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,8 +1,34 @@
 import { useState } from "react";
+import { IResource } from "../interfaces";
 
-export default function SearchBar(): JSX.Element {
+interface SearchBarProps{
+  resources: IResource[];
+}
 
+export default function SearchBar(props:SearchBarProps): JSX.Element {
+
+
+const resources = props.resources; 
 const [searchText, setSearchText] = useState<string>('')
+
+const filterResources = (searchedText:string, resources:IResource[]) => {
+  if (!searchText){
+    return resources;
+  }
+  
+  
+
+  const filteredList = resources.filter((resource) => 
+  {
+    
+    const allTags:string = resource.tags.join("#") //['react', 'javascript'] => #react#javascript so I can't searchactja
+
+    resource.author_name.toLowerCase().includes(searchText.toLowerCase()) ||
+    resource.resource_description.toLowerCase().includes(searchText.toLowerCase()) ||
+    resource.resource_name.toLowerCase().includes(searchText.toLowerCase()) ||
+    allTags.includes(searchText.toLowerCase())});
+  return [];
+}
 
   return (
     <input placeholder="Search for a resource." 
@@ -10,3 +36,15 @@ const [searchText, setSearchText] = useState<string>('')
     onChange={(e)=>{setSearchText(e.target.value)}} />
   );
 }
+
+
+// function findMatchingEps(message: string, filteredArr: IEpisode[]) {
+//   if (!message) {
+//     return filteredArr;
+//   }
+//   return filteredArr.filter(
+//     (episode) =>
+//       episode.name.toLowerCase().includes(message.toLowerCase()) ||
+//       episode.summary.toLowerCase().includes(message.toLowerCase())
+//   );
+// }
