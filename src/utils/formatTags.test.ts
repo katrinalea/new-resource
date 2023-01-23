6d1ebd47336ca2654f8b21040aca6940504a04c7
Maria <mtens17@gmail.com>
2023-01-23 13:59:12 +0000

    Format tags function and test

---
+++ src/utils/formatTags.test.ts
@@ -0,0 +1,7 @@
+const tags = ["{\"React\",\"Typescript\",\"Javascript\",\"Front-end\",\"Back-end\",\"CSS\",\"HTML\",\"SQL\"}"]
+
+import { formatTags } from "./formatTags"
+
+test("formatTags gets rid of curly braces and returns an array", ()=>{
+    expect(formatTags(tags)).toStrictEqual(["React", "Typescript", "Javascript", "Front-end","Back-end", "CSS", "HTML", "SQL"])
+})
---
+++ src/utils/formatTags.ts
@@ -0,0 +1,12 @@
+
+
+export function formatTags(tags: string[]): string[] {
+    const returnTagsBeg = tags[0].replace("{\"", "")
+    const returnTagsEnd = returnTagsBeg.replace("\"}", "")
+    const returnTags = returnTagsEnd.split("\",\"")
+    return returnTags
+
+}
+
+
+
