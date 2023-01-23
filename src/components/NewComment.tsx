480cafd4e5f081b9197b5cf3b522d210013d8de4
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-01-23 14:46:19 +0000

    Comment functionality added

--- src/components/NewComment.tsx
+++ src/components/NewComment.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { useState } from "react";
+import { url } from "../App";
 
 interface INewCommentProps {
     userID: number;
@@ -7,10 +9,23 @@ interface INewCommentProps {
 
 export default function NewComment(props: INewCommentProps): JSX.Element {
 
+const userID = props.userID
+const resourceID = props.resourceID
+
 const [commentText, setCommentText] = useState<string>("");
 
 async function handleSubmit(){
-    console.log("hi");
+    if (!commentText ){
+        window.alert("No comment text!")
+    } else {
+        await axios.post(`${url}/comments/${resourceID}/`,
+        { 
+            user_id: userID, 
+            resource_id: resourceID, 
+            comment: commentText 
+        }) //user.id, resource.id and comment text
+    }
+    console.log("posted: ", commentText);
 }
 
     return(
