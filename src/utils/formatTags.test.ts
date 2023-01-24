d38c1e55a3722690d195006ce3ba1df29d665585
Zac Gladman <zacharygladman@yahoo.co.uk>
2023-01-24 15:53:46 +0000

    place import at top of page, formatTagsTest

--- src/utils/formatTags.test.ts
+++ src/utils/formatTags.test.ts
@@ -1,8 +1,8 @@
+import { formatTags } from "./formatTags";
+
 const tags = [
   '{"React","Typescript","Javascript","Front-end","Back-end","CSS","HTML","SQL"}',
 ];
-
-import { formatTags } from "./formatTags";
 
 test("formatTags gets rid of curly braces and returns an array", () => {
   expect(formatTags(tags)).toStrictEqual([
