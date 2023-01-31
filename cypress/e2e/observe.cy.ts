a657790927907fb91d38323c1be08c34ddfe38a1
Maria <mtens17@gmail.com>
2023-01-31 10:08:03 +0000

    Add descriptions to cypress tests

--- cypress/e2e/observe.cy.ts
+++ cypress/e2e/observe.cy.ts
@@ -1,5 +1,5 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Homepage', () => {
+  it('Observes Home when visiting the homepage', () => {
     cy.visit('http://localhost:3000')
     cy.contains('Home')
   })
