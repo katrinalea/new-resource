05c3ebd39e8e4a8ab593d3b29b534336d8d15e9b
Maria <mtens17@gmail.com>
2023-01-31 13:53:38 +0000

    Fix typo and delete irrelevant test

--- cypress/e2e/press-btn.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Tag buttons', () => {
-  it('Checks tag buttons exist and can be clicked', () => {
-    cy.visit('http://localhost:3000/add-resource')
-    cy.get('.tag').click({multiple: true})
-  })
-})
--- cypress/e2e/visit.cy.ts
+++ cypress/e2e/visit.cy.ts
@@ -1,5 +1,5 @@
 describe('Web loading', () => {
-  it('CheckS that the website loads', () => {
+  it('Checks that the website loads', () => {
     cy.visit('http://localhost:3000')
   })
 })
